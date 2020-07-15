109f4856cd8957597df54e082d153bca93cdf317
zhanglizhi01 <zhanglizhi01@meicai.cn>
2020-07-15 19:25:29 +0800

    Merge branch 'master' of github.com:sparrowzoo/sparrow-pyalink
    # Conflicts:
    #	.idea/workspace.xml

--- source/sparrow/strategy/main.py
+++ source/sparrow/strategy/main.py
@@ -6,7 +6,6 @@ from sparrow.strategy.wide_deep_model import WideDeepModel
 
 model_registry.register(LRModel())
 model_registry.register(WideDeepModel())
-model_registry.register(DeepModel())
 #py model_name model_arg1 model_arg2  model_argn
 if __name__ == '__main__':
     model_registry.get(sys.argv[1]).run(sys.argv[2])
--- source/sparrow/xml_db/pyspark_config.py
+++ source/sparrow/xml_db/pyspark_config.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 ''''
 pyspark环境配置
 _SPARK_APP_NAME spark环境名字
-_SPARK_URL 使用yarn集群
+_SPARK_MASTER 使用yarn集群
 _SPARK_EXECUTOR_MEMORY 该参数设置的是每个EXECUTOR分配的内存的数量
 _SPARK_EXECUTOR_CORES 该参数为设置每个EXECUTOR能够使用的CPU core的数量。
 _SPARK_EXECUTOR_INSTANCES yarn集群中，最多能够同时启动的EXECUTOR的实例个数
@@ -14,7 +14,7 @@ _ENABLE_HIVE_SUPPORT 是否支持hive
 
 class Pyconfig(object):
     SPARK_APP_NAME = None
-    SPARK_URL = "yarn"
+    SPARK_MASTER = "local"
     SPARK_EXECUTOR_MEMORY = "4g"
     SPARK_EXECUTOR_CORES = 2
     SPARK_EXECUTOR_INSTANCES = 2
@@ -25,7 +25,7 @@ class Pyconfig(object):
         config = (
             ("spark.app.name", self.SPARK_APP_NAME),
             ("spark.executor.memory", self.SPARK_EXECUTOR_MEMORY),
-            ("spark.master", self.SPARK_URL),
+            ("spark.master", self.SPARK_MASTER),
             ("spark.executor.cores", self.SPARK_EXECUTOR_CORES),
             ("spark.executor.instances", self.SPARK_EXECUTOR_INSTANCES)
         )
